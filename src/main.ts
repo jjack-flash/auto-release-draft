264ebca41e9062b64c2b1b4247e793c73c78560b
jjack-flash <jjack.flash42@gmail.com>
2020-03-25 17:05:10 -0600

    Add missing git.ts impl

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import * as core from '@actions/core'
 import * as event from './event'
 import * as version from './version'
+import * as git from './git'
 
 export async function run(): Promise<void> {
   try {
-    const tag = event.getCreatedTag();
+    const tag = event.getCreatedTag()
 
     if (tag && version.isSemVer(tag)) {
-      //todo
+      const changelog = await git.getChangesIntroducedByTag(tag)
+
+      core.debug(`Detected changelog:\n${changelog}`)
     }
     core.setOutput('release-url', 'https://example.com')
   } catch (error) {
